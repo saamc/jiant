45f5bf990a7171be7a3199ed293a9e54b4c046b2
Jason Phang <email@jasonphang.com>
2018-10-22 15:31:38 -0400

    Add openai_nonlm_weight

--- src/trainer.py
+++ src/trainer.py
@@ -57,6 +57,7 @@ def build_trainer_params(args, task_names):
 
     params["openai_finetune_lm"] = getattr(args, "openai_finetune_lm")
     params["openai_lm_weight"] = getattr(args, "openai_lm_weight")
+    params["openai_nonlm_weight"] = getattr(args, "openai_nonlm_weight")
 
     return Params(params)
 
